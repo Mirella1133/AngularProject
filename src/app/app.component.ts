93d59b749e9ff4b1c1a2e1c26658ba3cc3b04519
MikeKronewald <mike.kronewald@atos.net>
2021-11-09 15:29:40 +0100

    ng if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Project1';
 
-  image_path: string[] = ["https://media.istockphoto.com/photos/studio-portrait-of-a-dog-sticking-out-her-tongue-picture-id1138848967?k=20&m=1138848967&s=612x612&w=0&h=JVCxh4GulisK2v4hVqhxRhZIvveld1RDXsCxRXhxYPk="];
+  image_path: string[] = ["https://cdn.pixabay.com/photo/2020/04/13/20/48/dog-5040008_960_720.jpg"];
 
   image_list:string=this.image_path[0];
   i=0;
-  id=2;
 
 
   clickFunction() {
@@ -24,7 +23,7 @@ export class AppComponent {
     }
 
     submit(){
-      var url=(document.getElementById("fname") as HTMLInputElement).value
+      var url=(document.getElementById("urlText") as HTMLInputElement).value
 
       if(!url.endsWith(".jpg",url.length)){
 
@@ -32,31 +31,8 @@ export class AppComponent {
       }
       else{
 
-      var td_id_node = document.createElement("td");
-
-      var id_text_node = document.createTextNode(this.id.toString());
-
-      td_id_node.appendChild(id_text_node);
-
-      var td_url_node = document.createElement("td");
-
-      var url_text_node = document.createTextNode(url);
-
-      td_url_node.appendChild(url_text_node);
-
-
-      var tr_node = document.createElement("tr");
-
-      tr_node.appendChild(td_id_node);
-      tr_node.appendChild(td_url_node);
-
-      var table_node = document.getElementsByTagName("table")[0];
-      table_node.appendChild(tr_node);
-
       this.image_path.push(url)
       this.image_list=url
-
-      this.id++;
       }
 
     }
