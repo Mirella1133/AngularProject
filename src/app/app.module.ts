c4701a469be6f90312168c9563f3615499e73c78
MikeKronewald <mike.kronewald@atos.net>
2021-11-10 10:02:40 +0100

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PictureComponent } from './picture/picture.component';
+import { InputComponent } from './input/input.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PictureComponent,
+    InputComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
